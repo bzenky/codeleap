2b2d58bcbedcf8d363044918738eaccbef3f72b6
Zenky <bzenky@gmail.com>
2023-04-07 16:11:29 -0300

    feat: static index page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
+import { Roboto } from 'next/font/google'
 import type { AppProps } from 'next/app'
+import '@/styles/globals.css'
+
+const roboto = Roboto({
+  weight: ['400', '500', '700'],
+  subsets: ['latin']
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={roboto.className}>
+      <Component {...pageProps} />
+    </main>
+
+  )
 }
