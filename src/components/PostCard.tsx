01b0070bfd373f8455dc635cd64cfbbed414aa94
Zenky <bzenky@gmail.com>
2023-04-08 07:42:34 -0300

    feat: static controls buttons when own post

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,5 +1,7 @@
 import { PostProps } from "@/pages/blog"
+import { usePostStore } from "@/store/posts"
 import { distanceToNow } from "@/utils/distanceToNow"
+import { PencilLine, Trash } from "@phosphor-icons/react"
 
 interface PostCardProps {
   post: PostProps
@@ -7,13 +9,26 @@ interface PostCardProps {
 
 export function PostCard({ post }: PostCardProps) {
   const created_at = new Date(post.created_datetime)
+  const username = usePostStore(state => state.username)
 
   return (
     <div key={post.id}>
-      <header className="flex items-center w-full  h-[70px] px-9 bg-[#7695EC] rounded-t-2xl">
+      <header className="flex items-center justify-between w-full  h-[70px] px-9 bg-[#7695EC] rounded-t-2xl">
         <h2 className="text-[22px] font-bold text-white">
           {post.title}
         </h2>
+
+        {username === post.username && (
+          <div className="flex gap-6">
+            <button>
+              <Trash size={24} color="#ffffff" />
+            </button>
+
+            <button>
+              <PencilLine size={24} color="#ffffff" />
+            </button>
+          </div>
+        )}
       </header>
 
       <div className="flex flex-col p-6 border border-[#999999] rounded-b-2xl">
