ff819cf2c53e6ee1f21010c6f526ce3c0c8616d3
Zenky <bzenky@gmail.com>
2023-04-07 23:08:42 -0300

    feat: create post

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,9 +1,9 @@
-import { GetServerSideProps } from "next/types"
 import { useEffect, useState } from "react"
-import { api } from "@/lib/axios"
+import { SpinnerGap } from "@phosphor-icons/react"
 import { PostCard } from "@/components/PostCard"
-import { usePostStore } from "@/store/posts"
 import { Loading } from "@/components/Loading"
+import { usePostStore } from "@/store/posts"
+import { api } from "@/lib/axios"
 
 export interface PostProps {
   id: number
@@ -25,11 +25,42 @@ export default function Blog() {
   const [content, setContent] = useState('')
   const [posts, setPosts] = useState<BlogProps>()
   const [isLoading, setIsLoading] = useState(true)
+  const [isFetching, setIsFetching] = useState(false)
+  const username = usePostStore(state => state.username)
 
   async function fetchPosts() {
-    await api.get<BlogProps>('')
-      .then(response => response.data)
-      .then(data => setPosts(data))
+    try {
+      await api.get<BlogProps>('')
+        .then(response => response.data)
+        .then(data => setPosts(data))
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async function handleCreatePost(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    try {
+      setIsFetching(true)
+
+      await api.post<PostProps>('', {
+        username,
+        title,
+        content
+      })
+        .then(() => {
+          setTitle('')
+          setContent('')
+          fetchPosts()
+        })
+
+      fetchPosts()
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setIsFetching(false)
+    }
   }
 
   useEffect(() => {
@@ -52,55 +83,58 @@ export default function Blog() {
             </header>
 
             <div className="flex flex-col gap-6 max-w-[800px] w-[94%] min-h-screen p-6 bg-white">
-              <>
-                <form className="flex flex-col border border-[#999999] p-6 rounded-2xl">
-                  <h2 className="mb-6 font-bold text-[22px]">
-                    What's on your mind ?
-                  </h2>
+              <form
+                className="flex flex-col border border-[#999999] p-6 rounded-2xl"
+                onSubmit={(event) => handleCreatePost(event)}
+              >
+                <h2 className="mb-6 font-bold text-[22px]">
+                  What's on your mind ?
+                </h2>
 
-                  <label htmlFor="title">
-                    Title
-                  </label>
+                <label htmlFor="title">
+                  Title
+                </label>
 
-                  <input
-                    id="title"
-                    className='w-full h-8 text-sm px-3 border border-[#777777] rounded-lg mt-2 mb-4'
-                    placeholder="Hello world"
-                    type="text"
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
+                <input
+                  id="title"
+                  className='w-full h-8 text-sm px-3 border border-[#777777] rounded-lg mt-2 mb-4'
+                  placeholder="Hello world"
+                  type="text"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+
+                <label htmlFor="">
+                  Content
+                </label>
+
+                <textarea
+                  id="content"
+                  placeholder="Content here"
+                  className='w-full h-[74px] text-sm py-2 px-3 border border-[#777777] rounded-lg mt-2 mb-4 resize-none'
+                  value={content}
+                  onChange={(e) => setContent(e.target.value)}
+                />
+
+                <button
+                  className="flex justify-center items-center w-[111px] h-[32px] bg-[#7695EC] text-white font-bold rounded-lg ml-auto hover:bg-[#5d7eda] transition-colors disabled:cursor-not-allowed disabled:opacity-50"
+                  disabled={title.trim().length === 0 || content.trim().length === 0}
+                >
+                  {isFetching
+                    ? <SpinnerGap size={24} className="animate-spin" color="#ffffff" />
+                    : 'Create'
+                  }
+                </button>
+              </form>
+
+              {posts?.results.map(post => {
+                return (
+                  <PostCard
+                    key={post.id}
+                    post={post}
                   />
-
-                  <label htmlFor="">
-                    Content
-                  </label>
-
-                  <textarea
-                    id="content"
-                    placeholder="Content here"
-                    className='w-full h-[74px] text-sm py-2 px-3 border border-[#777777] rounded-lg mt-2 mb-4 resize-none'
-                    value={content}
-                    onChange={(e) => setContent(e.target.value)}
-                  />
-
-                  <button
-                    className="w-[111px] h-[32px] bg-[#7695EC] text-white font-bold rounded-lg ml-auto hover:bg-[#5d7eda] transition-colors disabled:cursor-not-allowed disabled:opacity-50"
-                    disabled={title.trim().length === 0 || content.trim().length === 0}
-                  >
-                    Create
-                  </button>
-                </form>
-
-                {posts?.results.map(post => {
-                  return (
-                    <PostCard
-                      key={post.id}
-                      post={post}
-                    />
-                  )
-                })}
-              </>
-
+                )
+              })}
             </div>
           </>
         )
